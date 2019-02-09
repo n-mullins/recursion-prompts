d8d3f966838089f6f41bf87d1a46a364d63cc57f
Nate <nathanaelmullins@gmail.com>
2019-02-08 20:13:33 -0800

    Recursion Prompts
    Purpose of Assignment: Get comfortable with recursion by writing
    recursive solutions for commonly used functions
    
    Updated in this Build:
    -Added solutions for problems 1-5

--- src/recursion.js
+++ src/recursion.js
@@ -7,26 +7,57 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+	if (n === 0 || n === 1) {
+		return 1;
+	} else if (n < 0) {
+		return null;
+	}
+	return n * factorial(n-1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+	if (array.length === 0) {
+		return 0;
+	}
+	return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+	if (array.length === 0) {
+		return 0;
+	}
+	if (!Array.isArray(array[0])) {
+		return array[0] + arraySum(array.slice(1));
+	}
+	return arraySum(array[0]) + arraySum(array.slice(1));
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+	let abs = Math.abs(n);
+	if (abs === 0) {
+		return true;
+	} if (abs === 1 || abs === -1) {
+		return false;
+	}
+	return isEven(abs - 2);
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+	if (n === 0) {
+		return 0;
+	}
+	if (n < 0) {
+	   return n + 1 + sumBelow(n + 1);
+	}
+	return n - 1 + sumBelow(n - 1);
 };
 
 // 6. Get the integers within a range (x, y).
