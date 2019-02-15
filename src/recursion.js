7888ce754bce836612b70f08e83985156df75708
Nate <nathanaelmullins@gmail.com>
2019-02-14 16:45:40 -0800

    Recursion Prompts
    Purpose of Assignment: Get comfortable with recursion by writing
    recursive solutions for commonly used functions
    
    Updated in this Build:
    -Added solutions for problems 31-36

--- src/recursion.js
+++ src/recursion.js
@@ -438,6 +438,12 @@ var flatten = function(array) {
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
 var letterTally = function(str, obj) {
+	obj ? obj = obj : obj = {};
+	if (!str.length) {
+		return obj;
+	}
+	obj[str[0]] = obj[str[0]] + 1 || 1;
+	return letterTally(str.slice(1), obj);
 };
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
@@ -446,18 +452,41 @@ var letterTally = function(str, obj) {
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
 var compress = function(list) {
+	if (!list.length) {
+		return [];
+	}
+	if (list[0] === list[1]) {
+		return compress(list.slice(1))
+	}
+	return [list[0]].concat(compress(list.slice(1)));
 };
 
 // 33. Augment every element in a list with a new value where each element is an array
 // itself.
 // augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
 var augmentElements = function(array, aug) {
+	if (!array.length) {
+		return [];
+	}
+	if (!array[0].length) {
+		return [[aug]].concat(augmentElements(array.slice(1), aug));
+	}
+	let newElement = array[0];
+	newElement.push(aug);
+	return [newElement].concat(augmentElements(array.slice(1), aug));
 };
 
 // 34. Reduce a series of zeroes to a single 0.
 // minimizeZeroes([2,0,0,0,1,4]) // [2,0,1,4]
 // minimizeZeroes([2,0,0,0,1,0,0,4]) // [2,0,1,0,4]
 var minimizeZeroes = function(array) {
+	if (!array.length) {
+		return [];
+	}
+	if (array[0] === 0 && array[1] === 0) {
+		return minimizeZeroes(array.slice(1))
+	}
+	return [array[0]].concat(minimizeZeroes(array.slice(1)));
 };
 
 // 35. Alternate the numbers in an array between positive and negative regardless of
@@ -465,12 +494,58 @@ var minimizeZeroes = function(array) {
 // alternateSign([2,7,8,3,1,4]) // [2,-7,8,-3,1,-4]
 // alternateSign([-2,-7,8,3,-1,4]) // [2,-7,8,-3,1,-4]
 var alternateSign = function(array) {
+	if (!array.length) {
+		return [];
+	}
+	if (array.length === 1) {
+		if (array[0] < 0) {
+			return [-array[0]]
+		}
+		return [array[0]];
+	}
+	if (array[0] < 0) {
+		if (array[1] < 0) {
+			return [-array[0], array[1]].concat(alternateSign(array.slice(2)));
+		}
+		return [-array[0], -array[1]].concat(alternateSign(array.slice(2)));
+	} else if (array[0] >= 0) {
+		if (array[1] < 0) {
+			return [array[0], array[1]].concat(alternateSign(array.slice(2)));
+		}
+		return [array[0], -array[1]].concat(alternateSign(array.slice(2)));
+	}
 };
 
 // 36. Given a string, return a string with digits converted to their word equivalent.
 // Assume all numbers are single digits (less than 10).
 // numToText("I have 5 dogs and 6 ponies"); // "I have five dogs and six ponies"
 var numToText = function(str) {
+	if (!str.length) {
+		return '';
+	}
+	if (/\d/.test(str[0])) {
+		if (str[0] === '0') {
+			return 'zero' + numToText(str.slice(1));
+		} else if (str[0] === '1') {
+			return 'one' + numToText(str.slice(1));
+		} else if (str[0] === '2') {
+			return 'two' + numToText(str.slice(1));
+		} else if (str[0] === '3') {
+			return 'three' + numToText(str.slice(1));
+		} else if (str[0] === '4') {
+			return 'four' + numToText(str.slice(1));
+		} else if (str[0] === '5') {
+			return 'five' + numToText(str.slice(1));
+		} else if (str[0] === '6') {
+			return 'six' + numToText(str.slice(1));
+		} else if (str[0] === '7') {
+			return 'seven' + numToText(str.slice(1));
+		} else if (str[0] === '8') {
+			return 'eight' + numToText(str.slice(1));
+		}
+		return 'nine' + numToText(str.slice(1));
+	}
+	return str[0] + numToText(str.slice(1));
 };
 
 
